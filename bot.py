09e844f94a215e78fd0de11422590d48149a87ba
Milan Sony <milanfrom2001@gmail.com>
2022-12-28 11:51:49 +0530

    topics.py file created and function to get topics and add topics to talk about on omegele written

--- bot.py
+++ bot.py
@@ -6,6 +6,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
+from topics import new_topics
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 # To stop chrome from automatically closing
 options = webdriver.ChromeOptions()
@@ -21,3 +24,11 @@ link = 'https://www.omegle.com/'
 driver.maximize_window()
 driver.get(link)
 sleep(2)
+
+# Adding topics to talk about from topics.py
+def topics():
+  newtopicinput = driver.find_element(By.CLASS_NAME, "newtopicinput")
+  for topics in new_topics:
+    newtopicinput.send_keys(topics)
+    newtopicinput.send_keys(Keys.ENTER) 
+    sleep(1)
