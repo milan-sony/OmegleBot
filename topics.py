ae68972c54090e63e102722424d7618840f893aa
Milan Sony <milanfrom2001@gmail.com>
2022-12-31 16:13:14 +0530

    comments added in topics.py file

--- topics.py
+++ topics.py
@@ -1,1 +1,5 @@
+# You can change or add new topics to the list by putting the topics inside single quotes '' or double quotes "" seperated by comma
+
+# Eg: topics_list = ['kerala', 'india', 'tiktok'] 
+
 topics_list = ['kerala']
